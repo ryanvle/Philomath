268a032634d3b106f37b6edf45d8d5c842b81703
ryanvle <ryanvle@uw.edu>
2024-09-15 15:09:45 -0700

    Added spdlog and made macros

---
+++ Philomath/src/Philomath.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// For use by philomath applications
+
+#include "Philomath/Application.h"
+#include <stdio.h>
+#include "Philomath/Log.h"
+
+
+// --Entry Point--
+#include "Philomath/EntryPoint.h"
+// ---------------
