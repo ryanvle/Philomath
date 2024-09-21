30107d0e8d5fcedeae6b757062974f6ca42de256
ryanvle <ryanvle@uw.edu>
2024-09-20 17:57:18 -0700

    progress on event system

--- Philomath/src/Philomath/Events/Event.h
+++ Philomath/src/Philomath/Events/Event.h
@@ -32,4 +32,52 @@ namespace Philomath {
 		EventCategoryMouseButton		= BIT(4)
 	};
 
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+	class PHILOMATH_API Event
+	{
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_Handled = false;
+
+	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{
+		}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&mEvent);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
 }
--- Philomath/src/Philomath/Events/KeyEvent.h
+++ Philomath/src/Philomath/Events/KeyEvent.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Philomath {
+
+	class PHILOMATH_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+}
