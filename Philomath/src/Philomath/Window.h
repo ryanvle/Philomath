065095854baeafd573cbd2d85d96860d7ca7f70e
ryanvle <ryanvle@uw.edu>
2024-09-25 10:26:16 -0700

    Created an interface for a Window

--- Philomath/src/Philomath/Window.h
+++ Philomath/src/Philomath/Window.h
@@ -20,4 +20,22 @@ namespace Philomath {
 			{
 			}
 	};
+
+	class PM_API Window
+	{
+		using EventCallbackFn = std::function<void(Event&)>;
+
+		virtual ~Window() {}
+
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+		virtual void SetVSync(bool enabled) = 0;
+		virtual bool IsVSync() const = 0;
+
+		static Window* Create(const WindowProps& props = WindowProps());
+	};
 }
