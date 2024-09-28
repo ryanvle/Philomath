0412c7b11ae15520d51b27fc59f97f9f2bc0a539
ryanvle <ryanvle@uw.edu>
2024-09-27 20:24:36 -0700

    small code type of day

--- Philomath/src/Platform/Windows/WindowsWindow.h
+++ Philomath/src/Platform/Windows/WindowsWindow.h
@@ -5,7 +5,12 @@
 #include <GLFW/glfw3.h>
 
 namespace Philomath {
-	class WindowsWindow : public Window {
-
+	class WindowsWindow : public Window 
+	{
+	public:
+		WindowsWindow(const WindowProps& props);
+		virtual ~WindowsWindow();
+		
+		void OnUpdate() override;
 	};
 }
